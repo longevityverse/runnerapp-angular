9af752b0a89d4d40c91165e6554f825e79a42427
longevityverse <160720087+longevityverse@users.noreply.github.com>
2024-06-02 23:19:49 +0000

    fg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,9 +12,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,40 +21,39 @@ import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
 import { TaskService } from './task.service';
 import { ExcelImportService } from './excel-import.service';
-import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: 'tasks', component: TaskListComponent },
-    { path: 'tasks/:id', component: TaskDetailsComponent },
-  ];
+  { path: '', component: DashboardComponent },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/:id', component: TaskDetailsComponent },
+];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        TaskListComponent,
-        TaskDetailsComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatListModule,
-        MatCardModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatTableModule,
-        FormsModule,
-        ReactiveFormsModule,
-        RouterModule.forRoot(appRoutes)
-    ],
-    providers: [TaskService, ExcelImportService],
-    bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TaskListComponent,
+    TaskDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTableModule,
+    MatOptionModule,
+  ],
+  providers: [TaskService, ExcelImportService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
